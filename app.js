597661baba18a0edf54f23483514b88f0994849b
TamNhiDuong <yanxinyee@gmail.com>
2020-01-15 19:44:32 +0200

    yargs

---
+++ app.js
@@ -0,0 +1,56 @@
+//const fs = require('fs')
+//fs.writeFileSync('notes.txt', 'Hi, this is created by NodeJs')
+//fs.appendFileSync('notes.txt', 'Append txt by Node')
+const chalk = require('chalk')
+const getNotes = require('./notes')
+const yargs = require('yargs')
+
+yargs.version('1.1.0')
+
+
+//Add command
+yargs.command({
+    command: 'add',
+    describe: 'add notes',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+}).parse();
+//Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove notes',
+    handler: function() {
+        console.log('Removing notes')
+    }
+}).parse();
+//List command
+yargs.command({
+    command: 'list',
+    describe: 'list notes',
+    handler: function() {
+        console.log('Listing notes')
+    }
+}).parse();
+//Read command
+yargs.command({
+    command: 'read',
+    describe: 'read notes',
+    handler: function() {
+        console.log('Reading notes')
+    }
+}).parse();
+console.log(yargs.argv);
